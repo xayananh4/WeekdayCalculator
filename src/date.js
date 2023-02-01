8d6c125534e48f125be8d43566b1dbbafb1a6f58
viradeth <xayananh4@gmail.com>
2023-02-01 15:30:58 -0800

    first commit
    Co-authored-by: James Provance <jb24x7@gmail.com>

---
+++ src/date.js
@@ -0,0 +1,89 @@
+export default class Date {
+
+  constructor(mm, dd, yyyy) {
+    this.month = mm;
+    this.day = dd;
+    this.year = yyyy;
+  }
+
+  checkDate(mm, dd, yyyy) {
+    let month = parseInt(mm);
+    let day = parseInt(dd);
+    let year = parseInt(yyyy);
+    let ly = 0;
+    let result = '';
+    let correct_year = '';
+    let leap_year = '';
+    let boolean;
+
+    try {
+      let x = String(Math.abs(parseInt(yyyy)));
+      boolean = Number.isInteger(parseInt(x));
+    }
+    catch (err) {
+      boolean = false;
+    }
+    if (Number.isInteger(month) && Number.isInteger && boolean) {
+
+      if (year == 0) {
+        return "Year 0 does not exist";
+      }
+      else if (year < 0) {
+        year = year + 1;
+      }
+      if (day < 1) {
+        result = "Invalid Date";
+      }
+      else {
+        // LEAP YEAR CODE
+        let month_name = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+        let months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+        if (Number.isInteger(year / 4)) {
+          months[2] = 29;
+        }
+
+        let count = 0;
+        for (let i = 0; i < month; i++) {
+          count += months[i];
+        }
+        // LEAP YEAR CODE
+        year -= 1;
+
+        let days = year * 365 + Math.floor(year / 4);
+
+        let num = (count + day + days) % 7;
+        let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        if (year >= 0) {
+          correct_year = String(year + 1) + "CE";
+        }
+        else {
+          correct_year = String(Math.abs(year)) + "BCE";
+        }
+        if (months.includes(29)) {
+          leap_year = "It is a leap year.";
+          ly = 1;
+        }
+
+        else {
+          leap_year = "It is not a leap year.";
+        }
+
+        if (day <= months[month]) {
+          result = month_name[month - 1] + " " + String(day) + " " + correct_year + " is a " + weekday[num] + ".\nIt is week " + String(Math.ceil(day / 7)) + " of " + month_name[month - 1] + ".\nIt is day " + String(count + day) + " with " + String(365 + ly - (count + day)) + " days left in the year.\n" + leap_year;
+        }
+        else {
+          result = "Invalid date";
+        }
+      }
+    }
+    else {
+      result = "Check input value";
+    }
+    return result;
+
+  }
+
+}
+
--- src/index.js
+++ src/index.js
@@ -1,20 +1,61 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Triangle from './triangle.js';
-function handleTriangleForm() {
-  event.preventDefault();
-  document.querySelector('#response').innerText = null;
-  const length2 = parseInt(document.querySelector('#length2').value);
-  const length1 = parseInt(document.querySelector('#length1').value);
-  const length3 = parseInt(document.querySelector('#length3').value);
-  const triangle = new Triangle(length1, length2, length3);
-  const response = triangle.checkType();
-  const pTag = document.createElement("p");
-  pTag.append(response);
-  document.querySelector('#response').append(pTag);
-}
+import Date from './date.js';
 
-window.addEventListener("load", function() {
-  document.querySelector("#triangle-checker-form").addEventListener("submit", handleTriangleForm);
+
+window.addEventListener("load", function () {
+
+  let month = document.getElementById("text1");
+  let day = document.getElementById("text2");
+  let year = document.getElementById("text3");
+  let display = document.getElementById("Display");
+  let CE = this.document.getElementById("CE");
+  let BCE = this.document.getElementById("BCE");
+
+  month.onkeyup = function () {
+    let m = month.value;
+    m = m.slice(0, 2);
+    m = parseInt(m);
+    if (m > 12) {
+      m = "";
+    }
+    month.value = m;
+    m = parseInt(m);
+  };
+
+  day.onkeyup = function () {
+    let d = day.value;
+    d = d.slice(0, 2);
+    d = parseInt(d);
+    if (d > 31) {
+      d = "";
+    }
+    day.value = d;
+    d = parseInt(d);
+  };
+
+
+  year.onkeyup = function () {
+    let y = year.value;
+    y = y.slice(0, 4);
+    y = parseInt(y);
+    year.value = y;
+  };
+
+  CE.onclick = function () {
+    let m = month.value;
+    let d = day.value;
+    let y = year.value;
+    const newDate = new Date(m,d,y);
+    display.innerHTML = newDate.checkDate(m,d,y);
+  };
+
+  BCE.onclick = function () {
+    let m = month.value;
+    let d = day.value;
+    let y = year.value;
+    const newDate = new Date(m, d, y);
+    display.innerHTML = newDate.checkDate(m, d, 0 - y);
+  };
 });
