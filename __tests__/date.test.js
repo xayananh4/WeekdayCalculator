0fdb2ab1989ab70c8301ba961070ab07a583de52
viradeth <xayananh4@gmail.com>
2023-02-02 09:22:31 -0800

    test passed

--- __tests__/date.test.js
+++ __tests__/date.test.js
@@ -5,7 +5,7 @@ describe('Triangle', () => {
   test('should correctly create a date object with month day year', () => {
     const date = new Date();
     
-    expect(date.checkDate(1,1,2020)).toEqual(1,1,2020);
+    expect(date.checkDate(1,2,2023)).toEqual('January 2 2023CE is a Tuesday.\nIt is week 1 of January.\nIt is day 2 with 363 days left in the year.\nIt is not a leap year.');
 
   });
 });
